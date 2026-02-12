2e21340023cc909bda66699df24f695c37d6c556
jjtjp94 <jjtjp94@gmail.com>
2026-02-12 15:21:23 -0800

    Update default scale in PharmacyScheduler.jsx from 60 to 15

---
+++ PharmacyScheduler.jsx
@@ -0,0 +1,16 @@
+// Import statements
+import React, { useState } from 'react';
+
+const PharmacyScheduler = () => {
+    // Change default scale from 60 to 15
+    const [scale, setScale] = useState(15);
+
+    // Rest of the component code
+    return (
+        <div>
+            {/* Component content here */}
+        </div>
+    );
+};
+
+export default PharmacyScheduler;
